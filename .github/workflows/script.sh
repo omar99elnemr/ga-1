196a290e6979b6b03984c6dd81978187abb3f40a
omar99elnemr <omar99elnemr@gmail.com>
2025-07-26 13:18:42 +0300

    Upload Script

---
+++ .github/workflows/script.sh
@@ -0,0 +1,4 @@
+                    sudo apt update -y
+                    sudo apt install cowsay -y
+                    cowsay -f dragon "Run for cover, I am a DRAGON....RAWR" > dragon.txt
+                    cat dragon.txt
