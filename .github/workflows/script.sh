cb06bab9306340c11ea392c429f3fdc297121a29
omar99elnemr <omar99elnemr@gmail.com>
2025-07-26 13:37:06 +0300

    Update Script

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -1,4 +1,4 @@
                     sudo apt update -y
                     sudo apt install cowsay -y
-                    cowsay -f dragon "Run for cover, I am a DRAGON....RAWR" > dragon.txt
+                    cowsay -f dragon "Run for cover, I am a DRAGON....RAWR" >> dragon.txt
                     
